bbf20fe86bc4af86bffef3faf9657353591dee87
Muhand Jumah <muhandjumah@gmail.com>
2018-12-02 13:19:37 -0500

    initialized class

---
+++ src/neural-network/neural-network/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  neural-network
+//
+//  Created by Muhand Jumah on 12/2/18.
+//  Copyright © 2018 Muhand Jumah. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
